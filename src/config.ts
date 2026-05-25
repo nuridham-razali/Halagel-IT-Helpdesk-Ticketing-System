14ef0331bb3111d4ffbde851100844ec1f42873c
Idham_Razali98 <kazuya.takumi17@gmail.com>
2026-05-25 14:26:37 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const CONFIG = {
   GOOGLE_SHEET_ID:        "1HwvG9UVjlomNlBJiHVNFhKn3tLzs6iKidu0IYb3KZnI",
   GOOGLE_API_KEY:         "AIzaSyDGxZJRYXgtxteXKB41f-YQ4FP-8Pzxwrw",
-  APPS_SCRIPT_URL:        "https://script.google.com/macros/s/AKfycbwOdhCV1V3R4T01Ss3of0AUCKyG8Mwwnv3exd4LrloV2lDWE1UWm5CSuCpNqUfRV9nW/exec",
+  APPS_SCRIPT_URL:        "https://script.google.com/macros/s/AKfycbxNyof6Pea76pXuUSyJSVC4jcx1pxe6laalBK9JQpZ5k9enOleTrhQMR-7YBJx2kdg7/exec",
   ADMIN_EMAIL:            "admin.it@halagel.com.my",
   ADMIN_NAME:             "Muhammad Nur Idham Bin Razali",
   COMPANY_NAME:           "HALAGEL",
